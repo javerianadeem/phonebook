e57c5c1c21913c86f5014bca5478f6fc7d94294f
Javeria <javerianadeem03@gmail.com>
2019-05-24 17:04:31 +0500

    added delete functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var addPhone = document.querySelector('#phone-list')
 const form = document.querySelector('#add-phone-form')
 function createContact(doc) {
-    addPhone.innerHTML = addPhone.innerHTML + `<li data-id="doc.id">
-    <div class="right-align">
-    <img src="https://img.icons8.com/metro/20/000000/delete-sign.png" >
+    addPhone.innerHTML = addPhone.innerHTML + `<li data-id="${doc.id}">
+    <div class="right-align" >
+    <img src="https://img.icons8.com/metro/20/000000/delete-sign.png" id="cross">
     </div>
       <div class="collapsible-header">Name: ${doc.data().name}</div>
       <div class="collapsible-body">
@@ -12,6 +12,14 @@ function createContact(doc) {
       
       </div>
     </li>`
+    var cross = document.querySelector('#cross')
+    cross.addEventListener('click',(e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.parentElement.getAttribute('data-id');
+        db.collection('contacts').doc(id).delete()
+    })
+
+    
 }
 
 
